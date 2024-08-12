e5ecc9975258ade45201b717ff796eb35fe3e42f
Enrique Bris <cubanhenry5@gmail.com>
2024-08-12 15:44:52 -0400

    queue interface

---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module github.com/enriquebris/gocircularqueue/src
+
+go 1.22.6
---
+++ src/queue.go
@@ -0,0 +1,14 @@
+package src
+
+type CircularQueue interface {
+	// Capacity returns queue capacity
+	Capacity() int
+	// Length return total amount of enqueued items
+	Length() int
+	// Enqueue enqueues a new item into the queue
+	Enqueue(key string, value any) (rKey string, rValue any, err error)
+	// Update updates a key/value pair based on the given key
+	Update(key string, value any) error
+	// Get returns the key/value for a pair currently enqueued
+	Get(key string) (rValue any, err error)
+}
